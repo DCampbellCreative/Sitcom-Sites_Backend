1b9c388fb7cb6176abd4ebf8b5de15a546047e42
dcampbellcreative <dougcampbell1990@gmail.com>
2022-05-16 17:27:17 -0400

    find shows collection

--- index.js
+++ index.js
@@ -1,14 +1,72 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config({ path: "./config.env" });
 
 const app = express();
-const port = 3500;
 
-app.use(cors());
+app.use(express.json());
 
-app.get("/shows", (req, res) => {});
+const uri = process.env.ATLAS_URI;
 
-app.listen(port, () =>
-  console.log(`Hello world app listening on port ${port}!`)
-);
+const client = new MongoClient(uri);
+
+// app.get("/shows", (req, res) => {
+//   shows
+//     .find()
+//     .then((shows) => {
+//       res.status(201).json(shows);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Error: " + err);
+//     });
+// });
+
+// async function main() {
+/**
+ * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+ * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+ */
+// const uri = process.env.ATLAS_URI;
+
+// const client = new MongoClient(uri);
+
+// try {
+// Connect to the MongoDB cluster
+// await client.connect();
+
+// Make the appropriate DB calls
+//     await listDatabases(client);
+//   } catch (e) {
+//     console.error(e);
+//   } finally {
+//     await client.close();
+//   }
+// }
+
+// main().catch(console.error);
+
+// async function listDatabases(client) {
+//   databasesList = await client.db().admin().listDatabases();
+
+//   console.log("Databases:");
+//   databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+// }
+
+MongoClient.connect(uri, function (err, db) {
+  if (err) throw err;
+  var dbo = db.db("sitcom_sites");
+  dbo
+    .collection("shows")
+    .find()
+    .toArray(function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+});
+
+// listen for requests
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
+});
