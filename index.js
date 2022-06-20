3614b0d700bcb233943e4f8e96817059dddb0749
dcampbellcreative <dougcampbell1990@gmail.com>
2022-06-20 17:55:23 -0400

    fix error

--- index.js
+++ index.js
@@ -91,17 +91,17 @@ app.get("/users/:username", (req, res) => {
 });
 
 // get a user by username
-app.get("/users/:username", (req, res) => {
-  users
-    .findOne({ username: req.params.username })
-    .then((user) => {
-      res.json(user);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+// app.get("/users/:username", (req, res) => {
+//   users
+//     .findOne({ username: req.params.username })
+//     .then((user) => {
+//       res.json(user);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Error: " + err);
+//     });
+// });
 
 // update user
 app.put("/users/:username", (req, res) => {
